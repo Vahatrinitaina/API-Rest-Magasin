3cae075d9bc75a2b206bb11e599612f03a92c586
Vahatriniaina Ramaheninarivo <ramahatra@gmail.com>
2023-08-17 15:41:43 +0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const morgan = require('morgan');
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
-const { testDatabaseConnection, syncDatabase} = require('./src/db/sequelize');
+const { testDatabaseConnection, syncDatabase } = require('./src/db/sequelize');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Test de la connexion à la base de données
 testDatabaseConnection();
@@ -14,10 +13,13 @@ syncDatabase();
 
 app
     .use(favicon(__dirname + '/favicon.ico'))
-    .use(morgan('dev'))
     .use(bodyParser.json())
 
-    // Nous placerons ici nos futures points de terminaison
+app.get('/', (req, res) => {
+    res.json('Salut, deploiement REUSSI')
+})
+
+// Nous placerons ici nos futures points de terminaison
 require('./src/routes/findAllProduits')(app) // permet de trouver tous les produits
 
 require('./src/routes/findProduitByPk')(app)// permet de trouver un produit via son ID
@@ -28,9 +30,9 @@ require('./src/routes/deleteProduit')(app) // suppréssion d'un produit
 require('./src/routes/login')(app)// login au REST API
 
 // gestion d'erreur 404 par ici
-app.use(({res})=>{
+app.use(({ res }) => {
     const message = 'impossible de trouver la ressource demandee, corrigez votre url'
-    res.status(404).json({message})// permet de definir une statut, le  code correspond a l'erreur, 404: page not found
+    res.status(404).json({ message })// permet de definir une statut, le  code correspond a l'erreur, 404: page not found
 })
 
 app.listen(port, () => console.log(`Notre app tourne sur le port ${port}`))
